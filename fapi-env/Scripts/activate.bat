8b709c69251e3375bde981cbbd084c41cf9f1319
KhoeTrinh <kelvintrinh09@gmail.com>
2024-09-30 21:39:58 +0700

    oke

--- fapi-env/Scripts/activate.bat
+++ fapi-env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\nguye\OneDrive\Documents\LearnToCode\FastApi\fastApi_course\fastApi-env
+set VIRTUAL_ENV=C:\Users\nguye\OneDrive\Documents\LearnToCode\FastApi\fastApi_course\fapi-env
 
 if not defined PROMPT set PROMPT=$P$G
 
@@ -16,7 +16,7 @@ if defined _OLD_VIRTUAL_PROMPT set PROMPT=%_OLD_VIRTUAL_PROMPT%
 if defined _OLD_VIRTUAL_PYTHONHOME set PYTHONHOME=%_OLD_VIRTUAL_PYTHONHOME%
 
 set _OLD_VIRTUAL_PROMPT=%PROMPT%
-set PROMPT=(fastApi-env) %PROMPT%
+set PROMPT=(fapi-env) %PROMPT%
 
 if defined PYTHONHOME set _OLD_VIRTUAL_PYTHONHOME=%PYTHONHOME%
 set PYTHONHOME=
@@ -25,7 +25,7 @@ if defined _OLD_VIRTUAL_PATH set PATH=%_OLD_VIRTUAL_PATH%
 if not defined _OLD_VIRTUAL_PATH set _OLD_VIRTUAL_PATH=%PATH%
 
 set PATH=%VIRTUAL_ENV%\Scripts;%PATH%
-set VIRTUAL_ENV_PROMPT=(fastApi-env) 
+set VIRTUAL_ENV_PROMPT=(fapi-env) 
 
 :END
 if defined _OLD_CODEPAGE (
--- main.py
+++ main.py
@@ -3,5 +3,5 @@ from fastapi import FastAPI
 app = FastAPI()
 
 @app.get('/')
-def index() -> dict[str, str]: 
-    return {"data": {"name": "khoa"}}
+def index():
+    return {'message': 'Hello, FastAPI!'}
