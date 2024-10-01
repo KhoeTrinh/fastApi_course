4a206617020c560ec42602e890ce8a7dae6e1ccf
KhoeTrinh <kelvintrinh09@gmail.com>
2024-10-01 08:36:36 +0700

    123

--- main.py
+++ main.py
@@ -1,7 +1,34 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from enum import Enum
 
 app = FastAPI()
 
-@app.get('/')
-def index():
-    return {'message': 'Hello, FastAPI!'}
+class GenreURLChoices(Enum):
+    KHOA1 = 'khoaone'
+    KHOA2 = 'khoatwo'
+    KHOA3 = 'khoathree'
+    KHOA4 = 'khoafour'
+
+BANDS = [
+    {'id': 1, 'name': 'Khoa1', 'genre': 'Khoaone'},
+    {'id': 2, 'name': 'Khoa2', 'genre': 'Khoatwo'},
+    {'id': 3, 'name': 'Khoa3', 'genre': 'Khoathree'},
+    {'id': 4, 'name': 'Khoa4', 'genre': 'Khoafour'},
+]
+
+@app.get('/bands')
+def band() -> list[dict]:
+    return BANDS
+
+@app.get('/bands/{id}', status_code=201)
+def bandById(id: int) -> dict:
+    band = next((b for b in BANDS if b['id'] == id), None)
+    if band is None:
+        raise HTTPException(status_code=404, detail='Band not found');
+    return band
+
+@app.get('/bands/genres/{genre}', status_code=202)
+def bandGenreById(genre: GenreURLChoices) -> list[dict]:
+    return [
+        b for b in BANDS if b['genre'].lower() == genre.value
+    ]
